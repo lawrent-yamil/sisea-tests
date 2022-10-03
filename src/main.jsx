083f769fb5055c6979e00e0c6b797ad4bab6841a
novasuperior <lawrentvoli@gmail.com>
2022-10-03 15:58:15 -0600

    xd

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Catalogo from '/src/components/catalogo.jsx'
-import Encabezado from '/src/components/Encabezado.jsx'
-import Pie from '/src/components/pie.jsx'
-import HomePage from './HomePage.jsx'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import '/src/css/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Catalogo from "/src/components/catalogo.jsx";
+import Encabezado from "/src/components/Encabezado.jsx";
+import Pie from "/src/components/pie.jsx";
+import HomePage from "./HomePage.jsx";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "/src/css/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-      <Encabezado />
-      <HashRouter>
-        <Route 
-          path="/catalogo"
-          element={<Catalogo />}
-        />
-        <Route
-          path="/"
-          index
-          element={<HomePage />}
-        />
-        <Route path="*" element={<HomePage />} />
-      </HashRouter>
-      <Pie />
-  </BrowserRouter>
-) 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <Encabezado />
+    <Routes>
+      <Route path="/catalogo" element={<Catalogo />} />
+      <Route path="/" index element={<HomePage />} />
+      <Route path="*" element={<HomePage />} />
+    </Routes>
+    <Pie />
+  </HashRouter>
+);
